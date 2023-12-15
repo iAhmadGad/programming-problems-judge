4f53ae7e76efe9e9cc3bbf1b4df3f466f920b1bc
Ahmad Gad <70626969+iAhmadGad@users.noreply.github.com>
2023-12-16 00:27:55 +0300

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,10 +1,37 @@
 #include <iostream>
 #include <fstream>
-#include <stdio.h>
+#include <string>
+#include <sstream>
+#include <regex>
+#include <fstream>
+#include <streambuf>
+#include "Solution.h"
 
 using namespace std;
 
 int main()
 {
-	FILE output;
+	string input = "Write input here", answer = "Write correct answer here";
+
+	streambuf* standardCoutBuf = cout.rdbuf();
+
+	stringstream inputStream;
+	cout.rdbuf(inputStream.rdbuf());
+	cout << input;
+
+	cin.rdbuf(inputStream.rdbuf());
+
+	stringstream outputStream;
+	cout.rdbuf(outputStream.rdbuf());
+
+	MAIN();
+
+	string output(istreambuf_iterator<char>(outputStream), {});
+
+	cout.rdbuf(standardCoutBuf);
+
+	if(answer == output) cout << "Accepted";
+	else cout << "Wrong Answer";
+
+	return 0;
 }
